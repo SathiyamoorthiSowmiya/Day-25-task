fb446638f3cf9a0153d6e65e6451fd59b5b3769f
SathiyamoorthiSowmiya <Sowmimoorthi04@gmail.com>
2024-09-05 14:59:36 +0530

    initial

--- src/todoapp.jsx
+++ src/todoapp.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import TodoForm from "./todoform.jsx";
 import TodoList from "./TodoList";
-import TodoApp from "./todoapp.jsx";
+import Todoform from "./todoapp.jsx";
 import TodoForm from './todoform.jsx';
 
 
